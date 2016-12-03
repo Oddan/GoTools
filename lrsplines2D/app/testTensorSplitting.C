1f0786d27dd895c9d7d8b97d4c7dc47fe7135569
Odd <odd.andersen@sintef.no>
2016-12-04 00:50:30 +0100

    Replaced range_x and range_y with new structure

--- lrsplines2D/app/testTensorSplitting.C
+++ lrsplines2D/app/testTensorSplitting.C
@@ -15,6 +15,12 @@ using IntVec  = vector<int>;
 using Int3Vec = vector<array<int, 3>>;
 
 namespace {
+  // Structure to denote a subdomain
+  struct Subdomain {
+    IntPair range_x; // first and last knot along x-knotvec (ignoring multiplicities)
+    IntPair range_y; // first and last knot along y-knotvec (ignoring multiplicities)
+  };
+  
   int insert_segment(Mesh2D& m,
 		     Direction2D d,
 		     double pval,
@@ -31,15 +37,13 @@ namespace {
   // the range under consideration.
   Int3Vec missing_tensorgrid_segments(const Mesh2D& m,
 				      const Direction2D d,
-				      const IntPair range_x,  // x-range
-				      const IntPair range_y); // y-range
+				      const Subdomain& dom); 
 
   // count total number of missing segments/multiplicities to make the specified
   // subdomain into a full grid (not counting boundaries).
   struct MissingSegInfo {int num; Int3Vec missing_x; Int3Vec missing_y;};
   MissingSegInfo total_missing_tensorgrid_segments(const Mesh2D&m,
-						   const IntPair range_x,
-						   const IntPair range_y);
+						   const Subdomain& dom);
 
   // Suggest a split of the specified domain into two subdomains with fewer
   // total missing segments/multiplicities.  'bnd_mult' specifies the
@@ -65,21 +69,13 @@ namespace {
     int new_cost1;
     int new_cost2;
   };
-  DomainSplitDetail suggest_domain_split(const Mesh2D& m, const IntPair range_x,
-					 const IntPair range_y, const int bnd_mult);
+  DomainSplitDetail
+  suggest_domain_split(const Mesh2D& m, const Subdomain& dom, const int bnd_mult);
 
-  struct ConsecutiveSplit {
-    Direction2D d;
-    IntPair from;
-    IntPair to;
-  };
-  struct Subdomain {
-    IntPair ll; // (x,y)-indices of lower left corner
-    IntPair ur; // (x,y)-indices of upper right corner
-  };
-  tuple<vector<ConsecutiveSplit>, vector<Subdomain>> recursive_split(const Mesh2D& m, 
-								     const int bnd_mult);
-  
+  struct ConsecutiveSplit {Direction2D d; IntPair from; IntPair to;};
+  tuple<vector<ConsecutiveSplit>, vector<Subdomain>>
+  recursive_split(const Mesh2D& m, const int bnd_mult, const Subdomain dom = {{-1, -1}, {-1, -1}});
+								     
   
 } // end anonymous namespace
 
@@ -108,11 +104,11 @@ int main(int argc, char* argv[])
   // copy(n.begin(), n.end(), ostream_iterator<int, wchar_t>(wcout, L" "));
   // wcout<< endl;
 
-  int total = total_missing_tensorgrid_segments(m, {0, 6}, {0, 5}).num;
+  int total = total_missing_tensorgrid_segments(m, {{0, 6}, {0, 5}}).num;
 
   wcout << total << endl;
 
-  const auto info = suggest_domain_split(m, {0, 6}, {0,5}, 2);
+  const auto info = suggest_domain_split(m, {{0, 6}, {0,5}}, 2);
   wcout << L"Direction:  " << info.d << endl;
   wcout << L"Split ix:   " << info.ix << endl;
   wcout << L"Init cost:  " << info.init_cost << endl;
@@ -199,15 +195,14 @@ namespace {
   // ==========================================================================
   Int3Vec missing_tensorgrid_segments(const Mesh2D& m,
 				      const Direction2D d,
-				      const IntPair range_x, 
-				      const IntPair range_y) 
+				      const Subdomain& dom)
   // ==========================================================================
   {
     // determine active knots in the opposite direction (not all zero in the segment)
-    const auto d2_knots = active_knots(m, flip(d), range_x, range_y); 
+    const auto d2_knots = active_knots(m, flip(d), dom.range_x, dom.range_y); 
 
-    const IntPair r1  = (d == XFIXED) ? range_x : range_y; // d-direction
-    const IntPair r2 = (d == XFIXED) ? range_y : range_x;  // other direction
+    const IntPair r1 = (d == XFIXED) ? dom.range_x : dom.range_y; // d-direction
+    const IntPair r2 = (d == XFIXED) ? dom.range_y : dom.range_x;  // other direction
 
     Int3Vec result;
     result.reserve(max(r1.second - r1.first - 1, 1));
@@ -231,12 +226,11 @@ namespace {
 
   // ==========================================================================
   MissingSegInfo total_missing_tensorgrid_segments(const Mesh2D& m,
-						   const IntPair range_x,
-						   const IntPair range_y)
+						   const Subdomain& dom)
   // ==========================================================================
   {
-    const auto c1 = missing_tensorgrid_segments(m, XFIXED, range_x, range_y);
-    const auto c2 = missing_tensorgrid_segments(m, YFIXED, range_x, range_y);
+    const auto c1 = missing_tensorgrid_segments(m, XFIXED, dom);
+    const auto c2 = missing_tensorgrid_segments(m, YFIXED, dom);
 
     const auto add_fun = [](int cur, const array<int, 3>& a) {return cur + a[0];};
     const int missing =
@@ -276,25 +270,24 @@ namespace {
     
   // ==========================================================================
   DomainSplitDetail suggest_domain_split(const Mesh2D& m,
-				 const IntPair range_x,
-				 const IntPair range_y,
-				 const int bnd_mult)
+					 const Subdomain& dom,
+					 const int bnd_mult)
   // ==========================================================================
   {
     // Unless there are internal lines in both directions, there is no point in splitting
-    if (min(range_x.second - range_x.first, range_y.second - range_y.first) < 2)
+    if (min(dom.range_x.second - dom.range_x.first, dom.range_y.second - dom.range_y.first) < 2)
       return {XFIXED, -1, 0, 0, 0, 0}; // dummy values, except -1, which flags 'no split'
       
     // If we got here, we know that both direction has at least one internal line
-    const auto init = total_missing_tensorgrid_segments(m, range_x, range_y);
+    const auto init = total_missing_tensorgrid_segments(m, dom);
     const auto adder = [](int cur, const array<int, 3>& a) {return cur + a[0];};
     const int missing_x = accumulate(init.missing_x.begin(), init.missing_x.end(), 0, adder);
     const int missing_y = accumulate(init.missing_y.begin(), init.missing_y.end(), 0, adder);
 
     // Choose direction of split (the direction with fewest missing multiplicities
     const Direction2D d = (missing_x < missing_y) ? XFIXED : YFIXED; 
-    const IntPair range_d     = (d == XFIXED) ? range_x : range_y;
-    const IntPair range_other = (d == XFIXED) ? range_y : range_x;
+    const IntPair range_d     = (d == XFIXED) ? dom.range_x : dom.range_y;
+    const IntPair range_other = (d == XFIXED) ? dom.range_y : dom.range_x;
     const Int3Vec& missing_d  = (d == XFIXED) ? init.missing_x : init.missing_y;
 
     // Identify candidates for split (only those associated with t-junctions in
@@ -319,11 +312,11 @@ namespace {
 	const IntPair r1 {range_d.first, ix};
 	const IntPair r2 {ix, range_d.second};
 	const int new_cost1 = total_missing_tensorgrid_segments(m,
-								(d == XFIXED) ? r1 : range_other,
-								(d == XFIXED) ? range_other: r1).num;
+							   {(d == XFIXED) ? r1 : range_other,
+							    (d == XFIXED) ? range_other: r1}).num;
 	const int new_cost2 = total_missing_tensorgrid_segments(m,
-								(d == XFIXED) ? r2 : range_other,
-								(d == XFIXED) ? range_other: r2).num;
+							   {(d == XFIXED) ? r2 : range_other,
+							    (d == XFIXED) ? range_other: r2}).num;
 	return PerfInfo {init.num - (split_cost + new_cost1 + new_cost2),
 	                split_cost, new_cost1, new_cost2};
       });
@@ -340,9 +333,11 @@ namespace {
 
   // ==========================================================================
   tuple<vector<ConsecutiveSplit>, vector<Subdomain>>
-  recursive_split(const Mesh2D& m, const int bnd_mult)
+  recursive_split(const Mesh2D& m, const int bnd_mult, const Subdomain dom)
   // ==========================================================================
   {
-
+    vector<ConsecutiveSplit> v;
+    vector<Subdomain> s;
+    return make_tuple(v, s);
   }
 } // end anonymous namespace
